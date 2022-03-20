672c393deb5c75025d180fdf9f9660d7c1cd0be6
phawit-s <56498877+phawit-s@users.noreply.github.com>
2022-03-20 15:15:11 +0700

    Delete agnos directory

--- agnos/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({nextpage, buttontext}) =>{
-    return(
-        <div className="buttonbox" onClick={nextpage}>{buttontext}</div>
-    )
-}
-
-export default Button;
