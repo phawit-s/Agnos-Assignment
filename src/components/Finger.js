5d89faa3c5c1825787941626cf1229c278ca6a21
phawit-s <56498877+phawit-s@users.noreply.github.com>
2022-03-20 18:39:15 +0700

    Update Abs.js
    Update Finger.js

--- src/components/Finger.js
+++ src/components/Finger.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const Finger = () => {
     const [position, setPostion] = useState("")
@@ -39,8 +39,8 @@ const Finger = () => {
             <button onClick={() => allhighlight("dip")} style={{width:"35px", height: "25px", position:"absolute" , left:"213px", top:"205px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
             <button onClick={() => allhighlight("dip")} style={{width:"30px", height: "25px", position:"absolute" , left:"150px", top:"270px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
 
-            <img src={require("../assets/dip-highlight.png")} alt="abs" onClick={() => allhighlight("dip")} style={(position === "others" || position === "dip")   ?  showstyle : hidestyle} />
-            <img src={require("../assets/dip-active.png")} alt="abs" style={position === "dip" ?  showstyle : hidestyle} />
+            <img src={require("../assets/dip-highlight.png")} alt="finger" onClick={() => allhighlight("dip")} style={(position === "others" || position === "dip")   ?  showstyle : hidestyle} />
+            <img src={require("../assets/dip-active.png")} alt="finger" style={position === "dip" ?  showstyle : hidestyle} />
 
             {/* pip */}
             <button onClick={() => allhighlight("pip")} style={{width:"40px", height: "30px", position:"absolute" , left:"320px", top:"250px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
@@ -49,8 +49,8 @@ const Finger = () => {
             <button onClick={() => allhighlight("pip")} style={{width:"30px", height: "25px", position:"absolute" , left:"170px", top:"314px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
             <button onClick={() => allhighlight("pip")} style={{width:"40px", height: "30px", position:"absolute" , left:"430px", top:"370px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
 
-            <img src={require("../assets/pip-highlight.png")} alt="abs" onClick={() => allhighlight("pip")} style={(position === "others" || position === "pip")   ?  showstyle : hidestyle} />
-            <img src={require("../assets/pip-active.png")} alt="abs" style={position === "pip" ?  showstyle : hidestyle} />
+            <img src={require("../assets/pip-highlight.png")} alt="finger" onClick={() => allhighlight("pip")} style={(position === "others" || position === "pip")   ?  showstyle : hidestyle} />
+            <img src={require("../assets/pip-active.png")} alt="finger" style={position === "pip" ?  showstyle : hidestyle} />
 
             {/* mcp-highlight */}
             <button onClick={() => allhighlight("mcp")} style={{width:"45px", height: "30px", position:"absolute" , left:"320px", top:"335px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
@@ -58,12 +58,12 @@ const Finger = () => {
             <button onClick={() => allhighlight("mcp")} style={{width:"45px", height: "30px", position:"absolute" , left:"225px", top:"347px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
             <button onClick={() => allhighlight("mcp")} style={{width:"45px", height: "30px", position:"absolute" , left:"185px", top:"370px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
             <button onClick={() => allhighlight("mcp")} style={{width:"45px", height: "30px", position:"absolute" , left:"390px", top:"440px", cursor:"pointer", zIndex:2, opacity: 0 ,borderRadius: '40px'}} />
-            <img src={require("../assets/mcp-highlight.png")} alt="abs" onClick={() => allhighlight("mcp")} style={(position === "others" || position === "mcp")   ?  showstyle : hidestyle} />
-            <img src={require("../assets/mcp-active.png")} alt="abs" style={position === "mcp" ?  showstyle : hidestyle} />
+            <img src={require("../assets/mcp-highlight.png")} alt="finger" onClick={() => allhighlight("mcp")} style={(position === "others" || position === "mcp")   ?  showstyle : hidestyle} />
+            <img src={require("../assets/mcp-active.png")} alt="finger" style={position === "mcp" ?  showstyle : hidestyle} />
             
             {/* highlight all */}
             <button onClick={() => allhighlight("others")} style={{width:"350px", height: "50px", position:"absolute" , left:"120px", bottom:"80px", cursor:"pointer", zIndex:2, opacity: 0, borderRadius: '20px'}} />
-            <img src={require("../assets/others-highlight.png")} alt="abs"  style={position === "others" ?  showstyle : hidestyle} />
+            <img src={require("../assets/others-highlight.png")} alt="finger"  style={position === "others" ?  showstyle : hidestyle} />
             
         </div>
     )
